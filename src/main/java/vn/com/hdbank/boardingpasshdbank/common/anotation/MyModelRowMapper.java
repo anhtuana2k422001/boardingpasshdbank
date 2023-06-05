3b20c74b397aefffe1395d8f70ff65a99b52ef96
Thanh Cong <thanhcong04042001@gmail.com>
2023-06-05 10:08:24 +0700

    fix SonarLint using FieldUtils Apache Common Lang3

--- src/main/java/vn/com/hdbank/boardingpasshdbank/common/anotation/MyModelRowMapper.java
+++ src/main/java/vn/com/hdbank/boardingpasshdbank/common/anotation/MyModelRowMapper.java
@@ -1,7 +1,9 @@
 package vn.com.hdbank.boardingpasshdbank.common.anotation;
 
 import lombok.AllArgsConstructor;
+import lombok.NonNull;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.lang.reflect.Field;
@@ -12,43 +14,50 @@ import java.sql.SQLException;
 @AllArgsConstructor
 public class MyModelRowMapper<T> implements RowMapper<T> {
     private final Class<T> clazz;
+
     @Override
-    public T mapRow(ResultSet rs, int rowNum) throws SQLException {
+    public T mapRow(@NonNull ResultSet rs, int rowNum) throws SQLException {
         T model;
         try {
             model = clazz.getDeclaredConstructor().newInstance();
         } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
             throw new SQLException("Failed to instantiate model class.", e);
         }
+        setModelFields(rs, model);
+        return model;
+    }
+
+    private void setModelFields(ResultSet rs, T model) throws SQLException {
         for (Field field : clazz.getDeclaredFields()) {
             MyColumn myColumn = field.getAnnotation(MyColumn.class);
             if (myColumn != null) {
-                String columnName = myColumn.name();
-                if (StringUtils.isEmpty(columnName)) {
-                    columnName = field.getName();
-
-                }
+                String columnName = StringUtils.defaultIfBlank(myColumn.name(), field.getName());
                 String columnValue = rs.getString(columnName);
                 if (columnValue == null && !myColumn.nullable()) {
-                    throw new SQLException(StringUtils.join("Column ", columnName , " cannot be null.") );
+                    throw new SQLException(StringUtils.join("Column ", columnName, " cannot be null."));
                 }
                 if (columnValue != null && columnValue.length() > myColumn.length()) {
-                    throw new SQLException(StringUtils.join("Column " , columnName , " exceeded maximum length."));
+                    throw new SQLException(StringUtils.join("Column ", columnName, " exceeded maximum length."));
                 }
-                field.setAccessible(true);
                 try {
-                    if (field.getType() == String.class) {
-                        field.set(model, columnValue);
-                    } else if (field.getType() == Long.class) {
-                        field.set(model, Long.parseLong(columnValue));
-                    } else {
-                        throw new SQLException("Unsupported column type.");
-                    }
+                    FieldUtils.writeField(model, field.getName(), parseColumnValue(columnValue, field.getType()), true);
                 } catch (IllegalAccessException e) {
                     throw new SQLException("Failed to set column value.", e);
                 }
             }
         }
-        return model;
+    }
+
+    private Object parseColumnValue(String columnValue, Class<?> fieldType) throws SQLException {
+        if (columnValue == null) {
+            return null;
+        }
+        if (fieldType == String.class) {
+            return columnValue;
+        } else if (fieldType == Long.class) {
+            return Long.parseLong(columnValue);
+        } else {
+            throw new SQLException("Unsupported column type.");
+        }
     }
 }
